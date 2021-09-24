f8e91853e0404d98ba7cab3173abe5217bb52a6f
chenxiaonan0 <cxn0518@126.com>
2021-09-24 15:21:05 +0800

    3

--- src/main/java/com/cxn/demo/TestGitApplication.java
+++ src/main/java/com/cxn/demo/TestGitApplication.java
@@ -9,6 +9,7 @@ public class TestGitApplication {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
+		System.out.println(3);
 		SpringApplication.run(TestGitApplication.class, args);
 	}
 
